09d5a5e26f6bf3665a0cf031896ae0564146894e
VladPulyak <vlad.pulyak03@mail.ru>
2023-05-03 11:20:51 +0300

    Change name of field in vUserProgress

--- DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
@@ -1,6 +1,6 @@
 create or alter view vUserProgress
 as
-select UP.UserId, T.TaskId, TT.TaskTrackId, CONCAT(UP.FirstName, ' ', UP.LastName) as UserName, T.Name, TT.TrackNote, TT.TrackDate 
+select UP.UserId, T.TaskId, TT.TaskTrackId, CONCAT(UP.FirstName, ' ', UP.LastName) as UserName, T.Name as TaskName, TT.TrackNote, TT.TrackDate
 from UserTask as UT
 join UserProfiles as UP on UP.UserId = UT.UserId
 join Task as T on T.TaskId = UT.TaskId
