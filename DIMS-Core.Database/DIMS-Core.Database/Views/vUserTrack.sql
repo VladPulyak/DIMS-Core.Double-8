c79bc3352d9b3a60d56e188faf06ea3fb00b9786
VladPulyak <vlad.pulyak03@mail.ru>
2023-05-03 23:26:37 +0300

    Change realization of vUserTrack

--- DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
@@ -1,4 +1,7 @@
-create or alter view vUserTrack
+create or alter view vUserTrack 
 as
-select UserId, TaskId, TaskTrackId, UserName, Name as TaskName, TrackNote, TrackDate
-from vUserProgress
+select UP.UserId, T.TaskId, TT.TaskTrackId, T.Name as TaskName, TT.TrackNote, TT.TrackDate
+from UserTask as UT
+join UserProfiles as UP on UP.UserId = UT.UserId
+join Task as T on T.TaskId = UT.TaskId
+join TaskTrack as TT on TT.UserTaskId = UT.UserTaskId
