658a989abfa99ed4e288e6339999c07e35247637
VladPulyak <vlad.pulyak03@mail.ru>
2023-05-09 13:01:08 +0300

    Fix bugs After Dmitriy's review (Change vUserTrack)

--- DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
@@ -1,7 +1,6 @@
 create or alter view vUserTrack 
 as
-select UP.UserId, T.TaskId, TT.TaskTrackId, T.Name as TaskName, TT.TrackNote, TT.TrackDate
+select UT.UserId, T.TaskId, TT.TaskTrackId, T.Name as TaskName, TT.TrackNote, TT.TrackDate
 from UserTask as UT
-join UserProfiles as UP on UP.UserId = UT.UserId
 join Task as T on T.TaskId = UT.TaskId
 join TaskTrack as TT on TT.UserTaskId = UT.UserTaskId
