a69818536a806c9cbaff2a6e879d92fb5ee8996b
VladPulyak <vlad.pulyak03@mail.ru>
2023-05-17 20:04:34 +0300

    Change Repository class

--- DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/Repository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Repositories/Repository.cs
@@ -7,12 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DIMS_Core.DataAccessLayer.Repositories
 {
-    /// <summary>
-    ///     TODO: Task #1
-    ///     Implement all methods
-    ///     Generic Repository
-    /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
+
     public abstract class Repository<TEntity> : IRepository<TEntity>
         where TEntity : class
     {
@@ -30,42 +25,32 @@ namespace DIMS_Core.DataAccessLayer.Repositories
             return Set.AsQueryable();
         }
 
-        public Task<TEntity> GetById(int id)
+        public async Task<TEntity> GetById(int id)
         {
             if (id == 0)
             {
-                // TODO: Task #3
-                // Create custom exception for invalid arguments
-                // based on abstract class BaseException
-                // throw new AnyException(string paramName);
                 throw new InvalidArgumentException("You inputed invalid argument");
             }
 
-            // TODO: type must be adjusted to entity type accordingly
-            var objectFromDB = Set.FindAsync(id).AsTask();
+            var objectFromDB = await Set.FindAsync(id);
 
             if (objectFromDB is null)
             {
-                // TODO: Task #4
-                // Create custom exception for non existed object in database
-                // based on abstract class BaseException
-                // throw new AnyException(string methodName, string message);
+
             }
 
-            // RECOMMEND: It's better to create a helper static class for errors instead of throwing them
-            // Ask us if you stucked and it looks ridiculous for you
             return objectFromDB;
         }
 
-        public Task<TEntity> Create(TEntity entity)
+        public async Task<TEntity> Create(TEntity entity)
         {
-            return Task.Run(() => Set.Add(entity).Entity);
+            var obj = await Set.AddAsync(entity);
+            return obj.Entity;
         }
 
         public TEntity Update(TEntity entity)
         {
-            _context.Entry(entity).State = EntityState.Modified;
-            return entity;
+            return Set.Update(entity).Entity;
         }
 
         public async Task Delete(int id)
@@ -79,10 +64,6 @@ namespace DIMS_Core.DataAccessLayer.Repositories
             return _context.SaveChangesAsync();
         }
 
-        /// <summary>
-        ///     In most cases this method is not important because our context will be disposed by IoC automatically.
-        ///     But if you don't know where will use your service better to specify this method (example, class library).
-        /// </summary>
         public void Dispose()
         {
             _context?.Dispose();
